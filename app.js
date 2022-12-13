506a8bb29384d3e672248621f134df03372d2bd4
Santiago Bestoso <106178878+Bestoso@users.noreply.github.com>
2022-12-13 02:24:04 -0300

    entregable numero 2

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+
 class ProductManager {
     static contador = 0;
     constructor () {
         this.products = [];
+        this.path = './products.json';
     }
     addProduct(title, description, price, thumbnail, stock){
         this.products.push({
@@ -14,27 +17,43 @@ class ProductManager {
         })
         if (title === undefined || description === undefined || price === undefined || thumbnail === undefined || stock === undefined) return 'All fields are required'
         if (typeof price !== 'number') return 'Price must be a number'
-        return this.products;
+        if (typeof stock !== 'number') return 'Stock must be a number'
+        fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2));
+    }
+    updateProduct(id, title, description, price, thumbnail, stock){
+        const product = this.products.find(product => product.code === id);
+        if (product === undefined) return 'Not found'
+        if (title !== undefined) product.title = title;
+        if (description !== undefined) product.description = description;
+        if (price !== undefined) product.price = price;
+        if (thumbnail !== undefined) product.thumbnail = thumbnail;
+        if (stock !== undefined) product.stock = stock;
+        fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2));
     }
     removeProduct(id){
-        return this.products = this.products.filter(product => product.code !== id)
+        this.products = this.products.filter(product => product.code !== id);
+        fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2));
     }
     getProductById(id){
         const product = this.products.find(product => product.code === id);
         if (product === undefined) return 'Not found'
-        return product
+        return fs.readFileSync(this.path, 'utf-8');
     }
     getProducts(){
-        return this.products;
+        return fs.readFileSync(this.path, 'utf-8');
     }
 }
 
 const manager = new ProductManager;
 
-console.log(manager.addProduct('coca', 'coca cola descripcion', 500, 'imagecoca.jpg', 31));
-console.log(manager.addProduct('fanta', 'fanta descripcion', 450, 'imagefanta.jpg', 23));
+manager.addProduct('coca', 'coca cola descripcion', 500, 'imagecoca.jpg', 31);
+manager.addProduct('fanta', 'fanta descripcion', 450, 'imagefanta.jpg', 23);
 
-console.log(manager.getProductById(0)); // coca-cola object
-console.log(manager.removeProduct(1)); // removes fanta
+manager.updateProduct(0, 'coca', 'coca cola descripcion', 500, 'imagecoca.jpg', 10);
+manager.updateProduct(1, 'fanta', 'fanta descripcion', 450, 'imagefanta.jpg', 20);
 
-console.log(manager.getProducts())
+manager.removeProduct(0);
+
+console.log(manager.getProductById(1));
+
+console.log(manager.getProducts());
